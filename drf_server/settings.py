60252e9a2b19fdee573ad185c2208ba99e7a923a
onshore <onshorecoding9@gmail.com>
2021-11-22 19:56:05 +0900

    Fix: migrations 디렉토리 생성

--- drf_server/settings.py
+++ drf_server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 import dj_database_url
 from pathlib import Path
 
@@ -140,7 +141,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
